4e9640839c600d4220525f11815ca29f4538cd45
sokleng <sokleng@192-168-1-114.tpgi.com.au>
2020-04-29 18:00:23 +1000

    Tute 6 Production

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Dog", email: "dog@rmit.student.edu.au", password: "Dog1234", password_confirmation: "Dog1234")
+User.create!(name: "Cat", email: "cat@rmit.student.edu.au", password: "Cat1234", password_confirmation: "Cat1234")
+User.create!(name: "Bird", email: "bird@rmit.student.edu.au", password: "Bird1234", password_confirmation: "Bird1234")
+User.create!(name: "Fish", email: "fish@rmit.student.edu.au", password: "Fish1234", password_confirmation: "Fish1234")
+User.create!(name: "Cow", email: "cow@rmit.student.edu.au", password: "Cow1234", password_confirmation: "Cow1234")
