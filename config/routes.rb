3abc8c19ec2a6f87d74ced71c271110fd45fd0c3
limsokleng <limsokleng11@gmail.com>
2020-05-15 18:29:27 +1000

    re commit tute 8

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get 'users', to: 'users#index'
+  get '/users', to: 'users#index'
 
   resources :users
 
