e62082a62201f680faf849a990fcbe1c45814033
limsokleng <limsokleng11@gmail.com>
2020-05-15 17:55:41 +1000

    Tute 8 production

--- Gemfile
+++ Gemfile
@@ -65,5 +65,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap-sass', '3.3.7'
 gem 'rails-controller-testing'
-gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
+gem 'jquery-rails', '4.3.5'
 gem 'rails-ujs', '~> 0.1.0'
+gem 'faker', '1.7.3'
+gem 'will_paginate', '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(   name: "Example User",
+                email: "example@railstutorial.org",
+                password: "foobar",
+                password_confirmation: "foobar",
+                admin: true)
 
-User.create!(name: "Dog", email: "dog@rmit.student.edu.au", password: "Dog1234", password_confirmation: "Dog1234")
-User.create!(name: "Cat", email: "cat@rmit.student.edu.au", password: "Cat1234", password_confirmation: "Cat1234")
-User.create!(name: "Bird", email: "bird@rmit.student.edu.au", password: "Bird1234", password_confirmation: "Bird1234")
-User.create!(name: "Fish", email: "fish@rmit.student.edu.au", password: "Fish1234", password_confirmation: "Fish1234")
-User.create!(name: "Cow", email: "cow@rmit.student.edu.au", password: "Cow1234", password_confirmation: "Cow1234")
+99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    User.create!( name: name,
+                  email: email,
+                  password: password,
+                  password_confirmation: password  )
+end
